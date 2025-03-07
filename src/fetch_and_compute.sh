b2a3f316de054d625eaa44245b74c7ee58f5633b
pescaida <pescaida@gmail.com>
2025-03-07 18:47:01 +0100

    run su

--- src/fetch_and_compute.sh
+++ src/fetch_and_compute.sh
@@ -7,7 +7,7 @@ python3 "keep-fetching.py" --once
 cd ../../../../
 cd ./src/dist
 pwd
-python3 "recompute.py" 
+sudo python3 "recompute.py" 
 cd ..
 echo " "
 echo "Data fetched and computed. Please refresh localhost:8000 now"
