f343deae2c1f65200bd534feea90492f1ded9e83
pescaida <pescaida@gmail.com>
2025-03-07 18:27:47 +0100

    server diag compat

--- server_diag.sh
+++ server_diag.sh
@@ -4,7 +4,7 @@ echo "Diagnosing the following processes to be up:"
 echo ""
 ps aux | grep "caddy" | grep -v grep
 ps aux | grep "python3 ./keep-fetching.py" | grep -v grep
-ps aux | grep "python3 ./bucle.py" | grep -v grep
+ps aux | grep "python3 ./bucle" | grep -v grep
 echo ""
 echo "http.server, keep-fetching, bucle are the 3 processes"
 echo "that make up a successfully running server"
