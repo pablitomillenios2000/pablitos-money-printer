75c3147ac3c70a6ca6b0dfcc56c9b2d57a2eab30
pescaida <pescaida@gmail.com>
2025-03-07 18:30:11 +0100

    stop server compat

--- stop_server.sh
+++ stop_server.sh
@@ -5,7 +5,7 @@ echo ""
 echo "Leaving the webserver running"
 echo ""
 
-pkill -f "python3 ./bucle.py"
+pkill -f "python3 ./bucle"
 pkill -f "python3 ./keep-fetching.py"
 
 python3 /home/g1pablo_escaida1/pablitos-money-printer/src/python/binance/private/sell20_beta2.py
